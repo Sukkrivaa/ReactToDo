101436a53e32a57775bbe9bf6cbea976e30f3bea
Sukkrivaa Vijayan <sukkrivaavijayan@Sukkrivaas-MacBook-Pro.local>
2018-03-09 17:35:46 +0800

    TodoList tests

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,21 @@ var expect = require("expect");
 var TestUtils = require("react-addons-test-utils");
 
 var TodoList = require("TodoList");
+var Todo = require("Todo");
 
 describe("TodoList", () => {
   it("should exist", () => {
       expect(TodoList).toExist();
   });
+
+  it("should render 1 todo component for each todo item", () => {
+    var todos = [{id:1, text:"doSomething"},{id:2, text:"doSomethingElse"}];
+    var todolist = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todolist, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+  });
+
+
+
 });

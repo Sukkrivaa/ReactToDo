84bdaf71cbcdaed4e63caeb65990148c9163abff
Sukkrivaa Vijayan <sukkrivaavijayan@Sukkrivaas-MacBook-Pro.local>
2018-03-09 17:11:53 +0800

    created list items

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require("react-router");
-
+var TodoApp = require("TodoApp");
 
 // Load foundation
 $(document).foundation();
@@ -13,6 +13,6 @@ require('style!css!sass!applicationStyles');
 //Front-end-routing
 ReactDOM.render(
   //Initialising the router
-<p>Boilerplate 3</p>,
+<TodoApp/>,
   document.getElementById('app')
 );
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,38 @@
+var React = require("react");
+
+var TodoList = require("TodoList");
+
+var TodoApp = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: [
+        {
+          id: 1,
+          text: "Walk the dog"
+        },
+        {
+          id: 2,
+          text: "Mow the lawn"
+        },
+        {
+          id: 3,
+          text: "Clean the Yard"
+        },
+        {
+          id: 4,
+          text: "Do Homework"
+        }
+      ]
+    };
+  },
+  render: function(){
+    var {todos} = this.state;
+    return (
+      <div>
+        <TodoList todos={todos}/>
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp

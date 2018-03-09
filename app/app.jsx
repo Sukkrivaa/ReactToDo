7e62d69d3e35afd7cb640547ed2bce238b5665b8
Sukkrivaa Vijayan <sukkrivaavijayan@Sukkrivaas-MacBook-Pro.local>
2018-03-09 15:08:37 +0800

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require("react-router");
+
+
+// Load foundation
+$(document).foundation();
+
+//App css
+require('style!css!sass!applicationStyles');
+
+
+//Front-end-routing
+ReactDOM.render(
+  //Initialising the router
+<p>Boilerplate 3</p>,
+  document.getElementById('app')
+);

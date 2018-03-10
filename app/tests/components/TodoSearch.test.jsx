9b1cd0f39d8ef1a4872dfdf870bbdbcef7e192fd
Sukkrivaa Vijayan <sukkrivaavijayan@Sukkrivaas-MacBook-Pro.local>
2018-03-10 12:32:00 +0800

    TodoSearch for test

---
+++ app/tests/components/TodoSearch.test.jsx
@@ -0,0 +1,39 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var $ = require("jQuery");
+var expect = require("expect");
+var TestUtils = require("react-addons-test-utils");
+
+var TodoSearch= require("TodoSearch");
+
+describe("TodoSearch", () => {
+  it("should exist", () => {
+      expect(TodoSearch).toExist();
+  });
+
+  it("should call onSearch when the text is changed", () => {
+    var SearchText = "Dog";
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy} />);
+
+    todoSearch.refs.searchText.value = SearchText;
+    TestUtils.Simulate.change(todoSearch.refs.searchText);
+
+    expect(spy).toHaveBeenCalledWith(false, "Dog");
+  });
+
+
+
+
+
+  it("should call onSearch when the show-completed checkbox is changed", () => {
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+
+    todoSearch.refs.showCompleted.checked = true;
+    TestUtils.Simulate.change(todoSearch.refs.showCompleted); //Will only run the function when this happens
+
+    expect(spy).toHaveBeenCalledWith(true,"");
+  });
+
+});

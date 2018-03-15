8cefa68d4f0b279fab1bf9f8ab839b654f336027
Sukkrivaa Vijayan <sukkrivaavijayan@Sukkrivaas-MacBook-Pro.local>
2018-03-15 14:49:53 +0800

    Added more reducers

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,50 @@ describe("Reducers", () => {
       expect(res).toBe(false);
     });
   });
+
+  describe("todosReducer", () => {
+    it("should add new todo", () => {
+      var action = {
+        type: "ADD_TODO",
+        text: "Walk the dog"
+      }
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toBe(1);
+      expect(res[0].text).toBe(action.text);
+    });
+
+
+    it("should toggle todo when called", () => {
+      var todos = [{
+        id: 3547654 ,
+        text: "hjdasd",
+        completed: false,
+        createdAt: 23,
+        completedAt: undefined
+      },{
+      id: 62715686358,
+      text: "sdjhcgw",
+      completed: false,
+      createdAt: 832,
+      completedAt: undefined
+    }
+    ]
+
+    var action = {
+      type: "TOGGLE_TODO",
+      id: 62715686358
+    }
+
+    var res = reducers.todosReducer(df(todos), df(action));
+
+    expect(res[1].completed).toBe(true);
+    expect(res[1].completedAt).toBeA("number");
+
+    });
+
+  });
+
+
 });

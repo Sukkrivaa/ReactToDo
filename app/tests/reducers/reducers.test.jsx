1e7e2de3dad0651ab2afd35c3cee6783883903f5
Sukkrivaa Vijayan <sukkrivaavijayan@Sukkrivaas-MacBook-Pro.local>
2018-03-15 13:45:16 +0800

    Added Reducers and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,6 @@
 var expect = require("expect");
 var reducers = require("reducers");
+var df = require("deep-freeze-strict"); //To check for pure functions
 
 describe("Reducers", () => {
   describe("searchTextReducer", () => {
@@ -8,8 +9,21 @@ describe("Reducers", () => {
         type: "SET_SEARCH_TEXT",
         searchText: "DOG"
       }
-      var res = reducers.searchTextReducer("", action);
-      expect(res).toBe(action.searchText);
+      var res = reducers.searchTextReducer(df(""), df(action));
+      expect(res).toEqual(action.searchText);
+    });
+  });
+
+
+  describe("showCompletedReducer", () => {
+    it("showCompleted status gets flipped", () => {
+      var action = {
+        type: "TOGGLE_SHOW_COMPLETED"
+      }
+      var res = reducers.showCompletedReducer(df(false), df(action));
+      expect(res).toBe(true);
+      res = reducers.showCompletedReducer(df(res), df(action));
+      expect(res).toBe(false);
     });
   });
 });
